4cc57ba6367005a5e670ff8d855ee88f349219d1
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-01 13:58:35 +0100

    add variable and show it

--- index.php
+++ index.php
@@ -3,3 +3,31 @@ Creare una variabile con un paragrafo di testo a vostra scelta.
 Stampare a schermo il paragrafo e la sua lunghezza.
 Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare. -->
+
+
+<?php
+
+// variabili
+$speech = "Dunque, miei concittadini americani, non chiedete cosa il vostro paese può fare per voi, chiedete cosa potete fare voi per il vostro paese. Concittadini del mondo, non chiedete cosa l'America può fare per voi, ma cosa possiamo fare, insieme, per la libertà dell'uomo.";
+
+$lengthSpeech = strlen($speech);
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Words</title>
+</head>
+<body>
+    <div>
+         <p> <?php echo $speech;?> </p>
+        <span> La lunghezza del paragrafo è: <?php echo $lengthSpeech; ?> </span>
+    </div>
+</body>
+</html>
